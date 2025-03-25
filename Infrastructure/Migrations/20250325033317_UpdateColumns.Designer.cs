ddba6d9e8906c825672f2994d48a00418916eb90
Andrea <andreadh01@gmail.com>
2025-03-24 23:30:16 -0700

    feat: update db migrations

--- Infrastructure/Migrations/20250325033317_UpdateColumns.Designer.cs
+++ Infrastructure/Migrations/20250325033317_UpdateColumns.Designer.cs
@@ -4,6 +4,7 @@ using HogwartsAPI.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HogwartsAPI.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250325033317_UpdateColumns")]
+    partial class UpdateColumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -107,6 +110,7 @@ namespace HogwartsAPI.Infrastructure.Migrations
                         .HasColumnType("char(36)");
 
                     b.Property<string>("FirstName")
+                        .IsRequired()
                         .HasColumnType("longtext");
 
                     b.Property<Guid?>("HouseId")
---
+++ Infrastructure/Migrations/20250325033317_UpdateColumns.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HogwartsAPI.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Wizards_Houses_HouseId",
+                table: "Wizards");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "HouseId",
+                table: "Wizards",
+                type: "char(36)",
+                nullable: true,
+                collation: "ascii_general_ci",
+                oldClrType: typeof(Guid),
+                oldType: "char(36)")
+                .OldAnnotation("Relational:Collation", "ascii_general_ci");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Traits",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Traits_Name",
+                table: "Traits",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Wizards_Houses_HouseId",
+                table: "Wizards",
+                column: "HouseId",
+                principalTable: "Houses",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Wizards_Houses_HouseId",
+                table: "Wizards");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Traits_Name",
+                table: "Traits");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "HouseId",
+                table: "Wizards",
+                type: "char(36)",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                collation: "ascii_general_ci",
+                oldClrType: typeof(Guid),
+                oldType: "char(36)",
+                oldNullable: true)
+                .OldAnnotation("Relational:Collation", "ascii_general_ci");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Traits",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Wizards_Houses_HouseId",
+                table: "Wizards",
+                column: "HouseId",
+                principalTable: "Houses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
